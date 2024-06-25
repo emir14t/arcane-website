5d40621169429d204a34bee3c764f7a85946990b
Audry Darveau <audry.darveau@gmail.com>
2024-06-25 17:28:46 +0000

    fix landing page error

--- client/src/app/pages/landing-page/landing-page/landing-page.component.ts
+++ client/src/app/pages/landing-page/landing-page/landing-page.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent implements OnInit{
-  ngOnInit(): void {
-    document.addEventListener('scroll', function () {
-      const image = document.getElementById('circuits-img') as HTMLElement;
-      const scrollY = window.scrollY;
-      const imageContainerHeight = (document.querySelector('.img-container') as HTMLElement ).offsetHeight;
-
-      if (scrollY > imageContainerHeight) {
-          image.style.position = 'absolute';
-          image.style.top = imageContainerHeight + 'px';
-      } else {
-          // image.style.position = 'fixed';
-          image.style.top = '0';
-      }
-  });
-  }
+export class LandingPageComponent {
+  
 }
