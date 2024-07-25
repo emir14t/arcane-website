eada413d0dd8db22a282f48f5554cb2cc98a105f
Audry Darveau <audry.darveau@gmail.com>
2024-07-25 09:04:10 +0000

    everything works !!!!!

--- client/src/app/components/chart/chart.component.ts
+++ client/src/app/components/chart/chart.component.ts
@@ -20,16 +20,22 @@ export class ChartComponent implements OnInit, AfterViewInit {
   chart!: Chart;
 
   constructor(private transactionService: TransactionService) {
-    this.data1 = Array(STATS_WINDOW).fill(0);
-    this.data2 = Array(STATS_WINDOW).fill(1);
-    this.data3 = Array(STATS_WINDOW).fill(2);
-    this.data4 = Array(STATS_WINDOW).fill(3);  
+    // this.data1 = Array(STATS_WINDOW).fill(0);
+    // this.data2 = Array(STATS_WINDOW).fill(1);
+    // this.data3 = Array(STATS_WINDOW).fill(2);
+    // this.data4 = Array(STATS_WINDOW).fill(3);  
+    this.data1 = [];
+    this.data2 = [];
+    this.data3 = [];
+    this.data4 = [];
+
     this.time = Array.from({ length: STATS_WINDOW }, (_, i) => i);
   }
 
   ngOnInit(): void {
     this.transactionService.data$.subscribe(newData => {
       if (this.chart) {
+        console.log("update");
         this.updateChart(newData);
       }
     });
@@ -46,18 +52,27 @@ export class ChartComponent implements OnInit, AfterViewInit {
       this.data2.push(data[1]);
       this.data3.push(data[2]);
       this.data4.push(data[3]);
+      this.time.push(this.time[this.time.length - 1] + 1);
   
-      if (this.data1.length > 10) { // Assuming you want to keep the size at 10
+      if (this.data1.length > STATS_WINDOW) { // Assuming you want to keep the size at 10
         this.data1.shift();
         this.data2.shift();
         this.data3.shift();
         this.data4.shift();
+        this.time.shift();
+
+        if(this.chart.options.scales !== undefined){
+          if (this.chart.options.scales['x'] !== undefined) {
+            this.chart.options.scales['x'].max = (this.chart.options.scales['x'].max) as number + 1;
+          }
+        }
       }
-  
+      
       this.chart.data.datasets[0].data = this.data1;
       this.chart.data.datasets[1].data = this.data2;
       this.chart.data.datasets[2].data = this.data3;
       this.chart.data.datasets[3].data = this.data4;
+      this.chart.data.labels = this.time;
       this.chart.update();
     }
   }
@@ -127,7 +142,8 @@ export class ChartComponent implements OnInit, AfterViewInit {
             title: {
               display: true,
               text: 'Time interval'
-            }
+            },
+            max: STATS_WINDOW,
           },
           y: {
             display: true,
